962b7be982ecaafd2773e5afb45d7a44ae2b97ae
Hamza <harper969798@gmal.com>
2024-12-15 01:38:37 -0800

    first commit

---
+++ app/components/Buttons.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+const Button = ({value, onClick}) => {
+  return (
+    <>
+    <button onClick={() => onClick(value)} >
+        {value}
+    </button>
+    
+    </>
+  );
+};
+
+export default Button;
---
+++ app/components/Output.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Output = ({display}) => {
+  return (
+  <>
+  
+  <div className='w-full '>
+    <input className='w-full h-20 bg-gray-100' type="text" value={display} disabled/>
+  </div>
+  
+  
+  
+  
+  </>
+  )
+}
+
+export default Output
